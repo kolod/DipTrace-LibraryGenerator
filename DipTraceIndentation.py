b9eb440849b9fde771242745e70c5f354380028e
Oleksandr Kolodkin <alexandr.kolodkin@gmail.com>
2021-02-04 22:41:11 +0200

    Fix DipTraceIndentation

--- DipTraceIndentation.py
+++ DipTraceIndentation.py
@@ -5,13 +5,6 @@ from reHelper import getTag
 
 def DipTraceIndentation(library:str) -> str:
 	result = ''
-
-	exceptions = [
-		'Source',
-		'Shape',
-	]
-
-	tag = ''
 	tags = []
 
 	for line in library.splitlines():
@@ -19,43 +12,68 @@ def DipTraceIndentation(library:str) -> str:
 		line = line.strip()
 		identation = len(tags)
 
-		if line == '':
-			continue
+		# Skip empty lines
+		if line == '': continue
 
 		# End of file
 		elif line == '()':
 			tags.clear()
 			identation = 0
 
-		elif line == ')':
-			if len(tags):
-				if tags[-1] in exceptions:
-					tags.pop()
-					identation -= 1
-			tag = tags.pop()
-			identation -= 1
-
+		# Enter subobject
 		elif line.startswith('('):
 			tag = getTag(line)
 
-			if line.endswith(')'):
-				if tag in exceptions:
-					if len(tags):
-						if tag != tags[-1]:
-							tags.append(tag)
-						else:
-							identation -= 1
-					else:
-						tags.append(tag)
-
-			else:
+			if not line.endswith(')'):
 				tags.append(tag)
 
+			elif tag == 'Source':
+				tags.append(tag)
+
+			elif tag == 'pt':
+				if len(tags) > 1 and tags[-1] == 'PadMask_TopSegments':
+					identation += 1
+				if len(tags) > 1 and tags[-1] == 'PadMask_BotSegments':
+					identation += 1
+
+			# Exception for Shape object in pattern
+			elif tag == 'Shape':
+				if tags[-1] == 'Shape' and 'Pattern' in tags:
+					identation -= 1
+				elif tags[-1] == 'Shapes':
+					tags.append(tag)
+
+		# Leave subobject
+		elif line == ')':
+
+			if len(tags) == 0:
+				print('Warning unexpected ")"')
+				result += '  ' * identation + line + '\n'
+				continue
+
+			if tags[-1] == 'Shape' and 'Pattern' in tags:
+				tags.pop()
+				identation -= 1
+
+			tag = tags.pop()
+			identation -= 1
+
+
+
+		# Do indentation
 		result += '  ' * identation + line + '\n'
+
 	return result
 
 
 if __name__ == "__main__":
-	with open('test.asc', 'r', encoding="utf-8") as input:
-		with open('test_new.asc', 'w', encoding="utf-8") as output:
+
+	# Test component library
+	with open('test.component.asc', 'r', encoding="cp1251", errors='ignore') as input:
+		with open('test.component.new.asc', 'w', encoding="cp1251", errors='ignore') as output:
+			output.write(DipTraceIndentation(input.read()))
+
+	# Test pattern library
+	with open('test.pattern.asc', 'r', encoding="cp1251", errors='ignore') as input:
+		with open('test.pattern.new.asc', 'w', encoding="cp1251", errors='ignore') as output:
 			output.write(DipTraceIndentation(input.read()))
